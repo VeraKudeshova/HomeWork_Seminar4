097c2914d6167d0c07ea6fc8b1ec719a4ffe9a6a
Vera Kudeshova <BepaKy@yandex.ru>
2022-12-22 08:48:45 +0700

    Задача 3. Напишите программу, которая задаёт массив из 8 случайных чисел (-10, 10) и выводит их на экран.

---
+++ Task3_S4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из 8 случайных чисел (-10, 10) и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]        //6, 1, 33 -> [6, 1, 33]
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+int Method (int a)
+{
+    return numbers[a];
+}
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(-11, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.WriteLine("]");
+
